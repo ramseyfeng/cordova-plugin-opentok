406ec990957ce8330e8da77fd4543ef59a6191e7
Ramsey Feng <ramsey.feng@boehringer-ingelheim.com>
2021-01-20 16:04:56 +0800

    Upgrade opentok SDK from version 2.5 to 2.7wq

--- scripts/downloadiOSSDK.js
+++ scripts/downloadiOSSDK.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var IosSDKVersion = "OpenTok-iOS-2.15.3";
+    var IosSDKVersion = "OpenTok-iOS-2.17.1";
     var downloadFile = require('./downloadFile.js'),
         exec = require('./exec/exec.js'),
-        Q = context.requireCordovaModule('q'),
+        Q = require('q'),
         deferral = new Q.defer();
     console.log('Downloading OpenTok iOS SDK');
     downloadFile('https://s3.amazonaws.com/artifact.tokbox.com/rel/ios-sdk/' + IosSDKVersion + '.tar.bz2',
